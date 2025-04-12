56ca929449273b1552485e84fb01ff3eca1a4391
Nunu-DMG09 <davidmesta09@gmail.com>
2025-04-12 16:45:08 -0500

    agregando ejercicios

--- proyectos-php/consultoria-industrial/script.js
+++
@@ -1,9 +0,0 @@
-// PARA EL SCROL DE MI NAVEGACION
-window.addEventListener("scroll", function () {
-      const navbar = document.getElementById("mainNavbar");
-      if (window.scrollY > 50) {
-        navbar.classList.add("shrink");
-      } else {
-        navbar.classList.remove("shrink");
-      }
-    });
